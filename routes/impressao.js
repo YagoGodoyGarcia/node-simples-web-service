c60cd693f19b3ae841fa47596a3ce51e4575293b
Yago Garcia <Yago.Garcia@la.logicalis.com>
2019-04-02 12:36:40 -0300

    teste2

--- routes/impressao.js
+++ routes/impressao.js
@@ -32,6 +32,7 @@ router.post('/', async (req, res) => {
         console.log(body)
         res.send(body)
     }).catch((err) => {
+        console.log(err)
         res.status(404).send(err.message);
     })
 
