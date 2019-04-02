58c154912caf0dd77036127708353b73982ff6c2
Yago Garcia <Yago.Garcia@la.logicalis.com>
2019-04-02 12:20:26 -0300

    teste

--- routes/impressao.js
+++ routes/impressao.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request-promise');
+// const request = require('request-promise');
 
 
 router.post('/', async (req, res) => {
