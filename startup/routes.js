3a8fbdd544f09a91efadd88eb95a2abe2e99af04
Yago Garcia <Yago.Garcia@la.logicalis.com>
2019-04-02 11:55:16 -0300

    rota

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const inicio = require('../routes/inicio');
-const receber = require('../routes/receber');
+const impressao = require('../routes/impressao');
 module.exports = function(app) {
     app.use(express.json());
     app.use('/', inicio)
-    app.use('/api/teste', receber);
+    app.use('/api/impressao', impressao);
 }
